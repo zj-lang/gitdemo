f93cc00bd2f3ea597fc4b8b9f3738addce333c96
zj <1429582475@qq.com>
2020-10-11 11:20:29 +0800

    Initial commit

---
+++ src/main/java/com/qfedu/pojo/User.java
@@ -0,0 +1,37 @@
+package com.qfedu.pojo;
+
+/**
+ * projectName: gitdemo
+ *
+ * @author: 张剑
+ * time: 2020/10/11 0011 上午 11:04
+ * description:
+ */
+public class User {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
